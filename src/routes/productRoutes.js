60e9e868e09b5740328397685b6d6b9cda03b67d
leGromp12 <kochala.oleksandr@chnu.edu.ua>
2025-12-10 03:05:27 +0200

    fix: getProduct typo

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
-import {
-  getProducts,
-  createProduct,
-} from "../controllers/contentController.js";
+import { getProduct, createProduct } from "../controllers/contentController.js";
 import { validateProduct } from "../middleware/validate.js";
 
 const router = express.Router();
-router.get("/", getProducts);
+router.get("/", getProduct);
 router.post("/", validateProduct, createProduct);
 router.put("/:id", validateProduct, updateProduct); // or router.patch
 router.delete("/:id", deleteProduct);
