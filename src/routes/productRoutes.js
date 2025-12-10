18c7be5f25f6bdd4ee865901c9f4ace5e304aa5a
leGromp12 <kochala.oleksandr@chnu.edu.ua>
2025-12-10 03:15:46 +0200

    fix: productRoutes.js fixed routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,6 +5,6 @@ import { validateProduct } from "../middleware/validate.js";
 const router = express.Router();
 router.get("/", getProduct);
 router.post("/", validateProduct, createProduct);
-router.put("/:id", validateProduct, updateProduct); // or router.patch
-router.delete("/:id", deleteProduct);
+// router.put("/:id", validateProduct, updateProduct); // or router.patch
+// router.delete("/:id", deleteProduct);
 export default router;
